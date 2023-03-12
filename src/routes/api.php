9f232da2351b2432a4fcbc56fb56bf1c5f2780d7
rasmus <rasmussonntag@gmail.com>
2023-03-12 21:15:09 +0100

    add docker support

---
+++ src/routes/api.php
@@ -0,0 +1,35 @@
+<?php
+
+use App\Http\Controllers\ItemController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "api" middleware group. Make something great!
+|
+*/
+
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+    return $request->user();
+});
+
+
+Route::get("/test", function () {
+    return "Hello World";
+});
+
+
+Route::prefix("/items")->group(function () {
+    Route::get("/", [ItemController::class, "index"]);
+    Route::post("/", [ItemController::class, "store"]);
+    Route::get("/{id}", [ItemController::class, "show"]);
+    Route::put("/{id}", [ItemController::class, "update"]);
+    Route::delete("/{id}", [ItemController::class, "destroy"]);
+});
+
